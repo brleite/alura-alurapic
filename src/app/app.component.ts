7a2cd9e0983bead601110d81bef5aedc1e4534c3
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-07 10:35:23 -0300

    One Way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
+  description = 'Leão';
+  url = "https://cdn.leroymerlin.com.br/products/quadro_leao_bronze_100x100cm_90658085_0001_600x600.jpg";
 }
