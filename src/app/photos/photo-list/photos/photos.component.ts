6d804f88e46af6d47b6f0892d821e2604eee9193
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-08 10:33:29 -0300

    Alteração para atualizar a lista de rows no OnChanges

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Photo } from '../../photo/photo';
 
 @Component({
@@ -6,15 +6,18 @@ import { Photo } from '../../photo/photo';
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css']
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnChanges {
 
   @Input() photos: Photo[] = [];
   rows: any[] = [];
 
   constructor() { }
 
-  ngOnInit(): void {
-    this.rows = this.groupColumns(this.photos);
+  ngOnChanges(changes: SimpleChanges) {
+    // Verifica se houve mudança na inbound property photos. Caso não tenha ocorrido, ela não é setada em changes
+    if (changes.photos) {
+      this.rows = this.groupColumns(this.photos);
+    }
   }
 
   groupColumns(photos: Photo[]) {
