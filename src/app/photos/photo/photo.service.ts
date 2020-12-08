a25f39b46f59800d1acdb2e579a717505aaa1667
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-08 08:09:52 -0300

    Tipando o retorno da API

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Photo } from './photo';
 
 const API = 'http://localhost:3000';
 
@@ -14,6 +15,6 @@ export class PhotoService {
 
   listFromUser(userName: string) {
     return this.http
-    .get<Object[]>(API + '/' + userName + '/photos');
+    .get<Photo[]>(API + '/' + userName + '/photos');
   }
 }
