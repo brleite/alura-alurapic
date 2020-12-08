c53d50e9ad06dbe544ace439d2c5b13976230b7c
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-08 08:52:38 -0300

    Início da criação de rotas

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,11 +1,17 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
-  declarations: [ PhotoComponent, PhotoListComponent ],
-  imports: [ HttpClientModule ]
+  declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent ],
+  imports: [
+    HttpClientModule,
+    CommonModule
+  ]
 })
 export class PhotosModule {
 
