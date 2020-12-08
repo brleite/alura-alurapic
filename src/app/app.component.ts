fe028e8de9f5905de536433aaf2c66ca0f7d15d7
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-08 07:48:35 -0300

    Consultando backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,7 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  photos = [
+  photos: Object[] = [];
+
+  constructor(http:HttpClient) {
+    http
+      .get<Object[]>('http://localhost:3000/flavio/photos')
+      .subscribe(photos => this.photos = photos);
+  }
+
+  /* photos = [
     {
       url: "https://cdn.leroymerlin.com.br/products/quadro_leao_bronze_100x100cm_90658085_0001_600x600.jpg",
       description: "Leão"
@@ -16,5 +25,5 @@ export class AppComponent {
       url: "https://s2.glbimg.com/awP2KnJnzIYEWAVOYLFL4d2nG0I=/0x0:2400x1599/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2018/a/m/0T0EZ4QZqcrtzp9ZwNeg/2018-10-21t152242z-534837741-rc174f509210-rtrmadp-3-usa-zoo-lions.jpg",
       description: "Leoa"
     }
-  ];
+  ]; */
 }
