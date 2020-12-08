16f99026a6dcaaa9d6c1e4def5c44196643ea828
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-08 15:27:49 -0300

    Paginação

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -4,6 +4,7 @@ import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'ap-photo-list',
@@ -15,13 +16,18 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter: string = '';
   debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
 
   constructor(
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
   ) {}
 
   // Ocorre depois da instanciação e depois de receber as inbound properties
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data.photos;
     this.debounce
       .pipe(debounceTime(300))
@@ -32,4 +38,17 @@ export class PhotoListComponent implements OnInit, OnDestroy {
     this.debounce.unsubscribe();
   }
 
+  load() {
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        // Não funciona porque o componente não sabe que o this.photos mudou, já que a referência é a mesma
+        // this.photos.push(...photos);
+        this.photos = this.photos.concat(photos);
+
+        if (!photos.length) {
+          this.hasMore = false;
+        }
+      });
+  }
 }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -7,9 +7,10 @@ import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
 import { FilterByDescription } from './photo-list/filter-by-description.pipe';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 @NgModule({
-  declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent, FilterByDescription ],
+  declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent, FilterByDescription, LoadButtonComponent ],
   imports: [
     HttpClientModule,
     CommonModule
