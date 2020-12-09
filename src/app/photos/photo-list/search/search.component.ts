7cd307ce792566967e7a92e7a34d4ec50e261a6f
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-09 09:41:14 -0300

    Iniciando comunicação entre componentes

---
+++ src/app/photos/photo-list/search/search.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+@Component({
+  selector: 'ap-search',
+  templateUrl: './search.component.html'
+})
+export class SearchComponent implements OnInit, OnDestroy {
+  debounce: Subject<string> = new Subject<string>();
+
+  ngOnInit(): void {
+    this.debounce
+        .pipe(debounceTime(300))
+        // .subscribe(filter => this.filter = filter);
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
+}
