6a3d45fe547ff23eda08846ba434fa4979398b43
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-09 14:45:02 -0300

    Autenticação

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
   templateUrl: './signin.component.html'
@@ -8,7 +9,9 @@ export class SignInComponent implements OnInit {
 
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService) {
 
   }
 
@@ -17,5 +20,24 @@ export class SignInComponent implements OnInit {
       userName: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+
+  login() {
+    const userName = this.loginForm.get('userName').value;
+    const password = this.loginForm.get('password').value;
+
+    this.authService
+      .authenticate(userName, password)
+      .subscribe(
+        () => {
+          console.log('Autenticado');
+
+        },
+        err => {
+          console.log(err);
+
+          alert('Invalid username or password');
+          this.loginForm.reset();
+        });
   }
 }
