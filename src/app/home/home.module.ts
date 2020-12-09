e15bc8589b3c7df89387c4a3eedb4ee5aa042f9b
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-09 13:56:04 -0300

    Validação de form

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SignInComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
     SignInComponent
+  ],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule
   ]
 })
 export class HomeModule {
