6851ee5514659ee9b592e4792cd2f7883b9a1f7c
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-10 10:39:52 -0300

    Ajuste para identificar se o código está sendo renderizado no cliente ou servidor.

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,7 +1,8 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/core/auth.service';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
 
 @Component({
   templateUrl: './signin.component.html'
@@ -14,7 +15,8 @@ export class SignInComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService,
-    private router: Router) {
+    private router: Router,
+    private platformDetectorService: PlatformDetectorService) {
 
   }
 
@@ -42,7 +44,7 @@ export class SignInComponent implements OnInit {
 
           alert('Invalid username or password');
           this.loginForm.reset();
-          this.userNameInput.nativeElement.focus();
+          this.platformDetectorService.isPlatformBrowser() && this.userNameInput.nativeElement.focus();
         });
   }
 }
