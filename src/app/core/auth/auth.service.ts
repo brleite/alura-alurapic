c003c58eb0d43c10290b5fb0361ee7098df54752
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-11 07:58:06 -0300

    Utilização de token na autenticação

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators'
 
 const API_URL = 'http://localhost:3000';
 
@@ -10,7 +11,16 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
+  // O pipe permite a adição de operações que serão executadas entre o post e o subscribe.
+  // Ao executar o subscribe, é garantido que as operações do pipe já terão sido executadas.
+  // O observe: response permite acesso ao cabeçalho da resposta.
   authenticate(userName: string, password: string) {
-    return this.http.post(API_URL + '/user/login', { userName, password });
+    return this.http
+      .post(API_URL + '/user/login', { userName, password }, { observe: 'response' })
+      .pipe(tap(res => {
+        const authToken = res.headers.get('x-access-token');
+        console.log(`User ${userName} authenticated with token ${authToken}`);
+
+      }));
   }
 }
