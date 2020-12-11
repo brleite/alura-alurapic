f225ef50fefdd9705a5be76887decdc353381fa8
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-11 08:11:28 -0300

    Criação do TokenService

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs/operators'
+import { TokenService } from '../token/token.service';
 
 const API_URL = 'http://localhost:3000';
 
@@ -9,7 +10,9 @@ const API_URL = 'http://localhost:3000';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService) { }
 
   // O pipe permite a adição de operações que serão executadas entre o post e o subscribe.
   // Ao executar o subscribe, é garantido que as operações do pipe já terão sido executadas.
@@ -19,6 +22,7 @@ export class AuthService {
       .post(API_URL + '/user/login', { userName, password }, { observe: 'response' })
       .pipe(tap(res => {
         const authToken = res.headers.get('x-access-token');
+        this.tokenService.setToken(authToken);
         console.log(`User ${userName} authenticated with token ${authToken}`);
 
       }));
