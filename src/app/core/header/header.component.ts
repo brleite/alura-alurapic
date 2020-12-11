3317565b576bc6af519acbcdabfb36510f6e1043
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-11 09:29:48 -0300

    Implementação do logout

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from '../user/user';
 import { UserService } from '../user/user.service';
@@ -11,9 +12,16 @@ export class HeaderComponent {
   user$: Observable<User>;
   // user: User;
 
-  constructor(userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private router:Router) {
     this.user$ = userService.getUser();
 
     // this.user$.subscribe(user => this.user = user);
+  }
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['']);
   }
 }
--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -24,6 +24,11 @@ export class UserService {
     return this.userSubject.asObservable();
   }
 
+  logout() {
+    this.tokenService.removeToken();
+    this.userSubject.next(null);
+  }
+
   private decodeAndNotify() {
     const token = this.tokenService.getToken();
     const user =  jwtDecode(token) as User;
