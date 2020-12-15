96ae2ffba82357d6bee6ce71814ff01a4d56496d
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-15 14:36:53 -0300

    Preview Image

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
+import { PhotoModule } from '../photo/photo.module';
 import { PhotoFormComponent } from './photo-form.component';
 
 @NgModule({
@@ -13,7 +14,8 @@ import { PhotoFormComponent } from './photo-form.component';
     CommonModule,
     ReactiveFormsModule,
     VMessageModule,
-    RouterModule
+    RouterModule,
+    PhotoModule
   ]
 })
 export class PhotoFormModule {
