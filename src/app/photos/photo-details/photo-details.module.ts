819a2e4ac971a8108e0cd216d02eb7030bd7a6f2
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-16 13:24:14 -0300

    Ajustes no formulário e CSS de comentários

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { VMessageModule } from "src/app/shared/components/vmessage/vmessage.module";
 import { PhotoModule } from "../photo/photo.module";
 import { PhotoCommentsComponent } from "./photo-comments/photo-comments.component";
 import { PhotoDetailsComponent } from "./photo-details.component";
@@ -17,7 +19,9 @@ import { PhotoDetailsComponent } from "./photo-details.component";
   imports: [
     CommonModule,
     PhotoModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    VMessageModule
   ]
 })
 export class PhotoDetailsModule {
