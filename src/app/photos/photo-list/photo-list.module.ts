6086019914eea559f41eaa0c5d39ec1f0bd97fd6
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-16 09:34:32 -0300

    Parametrização da rota de photo details

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CardModule } from 'src/app/shared/components/card/card.module';
 import { DarkenOnHoverModule } from 'src/app/shared/directives/darken-on-hover/darken-on-hover.module';
 import { PhotoModule } from '../photo/photo.module';
@@ -21,7 +22,8 @@ import { SearchComponent } from './search/search.component';
     CommonModule,
     PhotoModule,
     CardModule,
-    DarkenOnHoverModule
+    DarkenOnHoverModule,
+    RouterModule
   ]
 })
 export class PhotoListModule {
