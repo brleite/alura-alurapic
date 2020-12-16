0d8c9910df29fc70aabaafd05d405d704406bd91
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-16 09:52:48 -0300

    Implementação de photo details

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { PhotoModule } from "../photo/photo.module";
 import { PhotoDetailsComponent } from "./photo-details.component";
 
 @NgModule({
   declarations: [PhotoDetailsComponent],
   exports: [PhotoDetailsComponent],
-  imports: [CommonModule]
+  imports: [
+    CommonModule,
+    PhotoModule
+  ]
 })
 export class PhotoDetailsModule {
 
