830816a095c2ad37b28ffbdcd6a0504498ba75cb
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-17 13:46:57 -0300

    Diretiva para mostrar elements somente se o usuári estiver logado

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { VMessageModule } from "src/app/shared/components/vmessage/vmessage.module";
+import { ShowIfLoggedModule } from "src/app/shared/directives/show-if-logged/show-if-logged.module";
 import { PhotoModule } from "../photo/photo.module";
 import { PhotoCommentsComponent } from "./photo-comments/photo-comments.component";
 import { PhotoDetailsComponent } from "./photo-details.component";
@@ -23,7 +24,8 @@ import { PhotoOwnerOnlyDirective } from "./photo-owner-only/photo-owner-only.dir
     PhotoModule,
     RouterModule,
     ReactiveFormsModule,
-    VMessageModule
+    VMessageModule,
+    ShowIfLoggedModule
   ]
 })
 export class PhotoDetailsModule {
