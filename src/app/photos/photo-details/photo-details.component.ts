d9100fdaaebc000d240a00a19129336a7c10bb0e
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-18 08:13:25 -0300

    Tratamento de erro da funcionalidade de curtida

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -46,4 +46,16 @@ export class PhotoDetailsComponent implements OnInit {
           this.alertService.warning('Could not delete the photo!', true);
         });
   }
+
+  like(photo: Photo) {
+    this.photoService
+      .like(photo.id)
+      .subscribe(
+        liked => {
+          if (liked) {
+            this.photo$ = this.photoService.findById(photo.id);
+          }
+        },
+        err => alert(err));
+  }
 }
