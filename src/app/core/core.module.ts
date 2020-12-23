d258f3eaf688289c89f24487859911e8002ca27f
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-23 08:21:05 -0300

    Início da implementação do menu

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,7 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AlertModule } from '../shared/components/alert/alert.module';
+import { MenuModule } from '../shared/components/menu/menu.module';
 import { LoadingModule } from '../shared/loading/loading.module';
 import { RequestInterceptor } from './auth/request.interceptor';
 import { FooterComponent } from './footer/footer.component';
@@ -17,7 +18,8 @@ import { HeaderComponent } from './header/header.component';
     CommonModule,
     RouterModule,
     AlertModule,
-    LoadingModule
+    LoadingModule,
+    MenuModule
   ],
   exports: [
     HeaderComponent,
