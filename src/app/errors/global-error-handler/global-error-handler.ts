9235bd37d978b669f785d4c44bbc824d785e96b7
Bruno Mayerle Leite <brleite@gmail.com>
2020-12-23 13:33:59 -0300

    Padronização da exibição do stack trace

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,9 +1,13 @@
 import { ErrorHandler } from "@angular/core";
+import * as StackTrace from 'stacktrace-js';
 
 export class GlobalErrorHandler implements ErrorHandler {
   handleError(error: any): void {
     console.log('passei pelo handler');
-
-    throw error;
+    StackTrace
+      .fromError(error)
+      .then(stackFrames => {
+        const stackAsString = stackFrames.map(sf => sf.toString()).join('\n');
+      });
   }
 }
